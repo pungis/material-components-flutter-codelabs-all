6a7f14d93acc5efff5581ad8e27351267ed98bc4
pungis <filip.bsl@gmail.com>
2019-12-05 21:41:10 +0100

    Add a menu on the back layer

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -20,11 +20,13 @@ import 'supplemental/asymmetric_view.dart';
 
 class HomePage extends StatelessWidget {
   // TODO: Add a variable for Category (104)
+  final Category category;
+
+  const HomePage({this.category: Category.all});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Return an AsymmetricView (104)
-    return  AsymmetricView(products: ProductsRepository.loadProducts(Category.all));
     // TODO: Pass Category variable to AsymmetricView (104)
+    return AsymmetricView(products: ProductsRepository.loadProducts(category));
   }
 }
